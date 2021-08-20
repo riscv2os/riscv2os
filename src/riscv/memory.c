e276ffa6d51adaddb1520b9fd2bd1544c7e7af31
ccckmit <ccckmit@gmail.com>
2021-08-20 17:11:43 +0800

    memory => chunks

--- src/riscv/memory.c
+++ src/riscv/memory.c
@@ -1,59 +1,73 @@
 #include "memory.h"
 
-#define MB      1024*1024
-#define NMEMORY 128*MB      // 最大記憶體範圍不可超過 128M
+#define NCHUNK  128
 
-static char memory[NMEMORY];
-static uint32_t mlow, mhigh;
+struct Chunk {
+    char *mem;
+    uint32_t begin, size, end;
+};
+
+static struct Chunk chunk[NCHUNK];
+static int    nchunk = 0;
 
 bool mem_load_elf(elf_t *e) {
     // rv_set_pc(rv, e->hdr->e_entry); /* set the entry point */
-    uint32_t low, high; // 找出最小與最大的記憶體位址，記憶體在此範圍內才是合法的 (這樣也可以少宣告些)
-    /* loop over all of the program headers */
-    for (int p = 0; p < e->hdr->e_phnum; ++p) {
-        /* find next program header */
-        uint32_t offset = e->hdr->e_phoff + (p * e->hdr->e_phentsize);
-        const struct Elf32_Phdr *phdr = (const struct Elf32_Phdr *) (e->rawdata + offset);
-        /* check this section should be loaded */
+    nchunk = e->hdr->e_phnum;
+    for (int i = 0; i < e->hdr->e_phnum; ++i) {
+        struct Elf32_Phdr *phdr = &e->phdrs[i];
         if (phdr->p_type != PT_LOAD)
             continue;
-        
-        if (phdr->p_vaddr < low) low = phdr->p_vaddr;
-
-        int pend = phdr->p_vaddr + max(phdr->p_memsz, phdr->p_filesz);
-        if (pend > high) high = pend;
+        struct Chunk *c = &chunk[nchunk++];
+        c->begin = phdr->p_vaddr;
+        c->size = max(phdr->p_memsz, phdr->p_filesz);
+        c->end = c->begin+c->size;
+        c->mem = malloc(c->size);
+        memcpy(c->mem, e->rawdata + phdr->p_offset, c->size);
     }
-    mlow = low;
-    mhigh = high;
     return true;
 }
 
+struct Chunk *mem_find_chunk(uint32_t addr) {
+  for (int i=0; i<nchunk; i++) {
+      struct Chunk *c = &chunk[i];
+      if (addr >= c->begin && addr < c->end)
+        return c;
+  }
+  return NULL;
+}
+
 uint32_t mem_read_w(uint32_t addr) {
-    if (addr+3 >= mhigh) error();
-    return *(uint32_t*) &memory[addr-mlow];
+    struct Chunk *c = mem_find_chunk(addr);
+    if (!c || addr+3 >= c->end) error();
+    return *(uint32_t*) &c->mem[addr-c->begin];
 }
 
 uint16_t mem_read_s(uint32_t addr) {
-    if (addr+1 >= mhigh) error();
-    return *(uint16_t*) &memory[addr-mlow];
+    struct Chunk *c = mem_find_chunk(addr);
+    if (!c || addr+1 >= c->end) error();
+    return *(uint16_t*) &c->mem[addr-c->begin];
 }
 
 uint8_t mem_read_b(uint32_t addr) {
-    if (addr >= mhigh) error();
-    return *(uint8_t*) &memory[addr-mlow];
+    struct Chunk *c = mem_find_chunk(addr);
+    if (!c || addr >= c->end) error();
+    return *(uint8_t*) &c->mem[addr-c->begin];
 }
 
 uint32_t mem_write_w(uint32_t addr, uint32_t data) {
-    if (addr+3 >= mhigh) error();
-    *(uint32_t*) &memory[addr-mlow] = data;
+    struct Chunk *c = mem_find_chunk(addr);
+    if (!c || addr+3 >= c->end) error();
+    *(uint32_t*) &c->mem[addr-c->begin] = data;
 }
 
 uint16_t mem_write_s(uint32_t addr, uint16_t data) {
-    if (addr+1 >= mhigh) error();
-    *(uint16_t*) &memory[addr-mlow] = data;
+    struct Chunk *c = mem_find_chunk(addr);
+    if (!c || addr+1 >= c->end) error();
+    *(uint16_t*) &c->mem[addr-c->begin] = data;
 }
 
 uint8_t mem_write_b(uint32_t addr, uint8_t data) {
-    if (addr >= mhigh) error();
-    *(uint8_t*) &memory[addr-mlow] = data;
+    struct Chunk *c = mem_find_chunk(addr);
+    if (!c || addr >= c->end) error();
+    *(uint8_t*) &c->mem[addr-c->begin] = data;
 }
